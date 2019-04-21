edb3100708188d1c78b374528a8f45b19c504534
ShaoJenChen <cash3310@outlook.com>
2019-04-21 13:01:28 +0800

    update

--- Minesweeper/MineCell.swift
+++ Minesweeper/MineCell.swift
@@ -12,16 +12,79 @@ class MineCell: UICollectionViewCell {
     
     @IBOutlet weak var mineCountLabel: UILabel!
     
+    @IBOutlet weak var mineBackView: UIView!
+    
+    @IBOutlet weak var mineIcon: UIImageView!
+    
+    @IBOutlet weak var flagIcon: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.flagIcon.isHidden = true
+        
+        self.mineIcon.isHidden = true
+        
+        self.mineBackView.isHidden = true
+        
+        self.isSwept = false
+        
+        self.contentView.backgroundColor = UIColor(red: 192.0/255.0, green: 192.0/255.0, blue: 192.0/255.0, alpha: 1)
+        
+    }
+    
     var mineCount: Int = 0 {
         willSet {
             
             self.mineCountLabel.text = String(newValue)
             
+            self.mineBackView.backgroundColor = MineCell.getBackColor(mineNumber: newValue)
+            
         }
     }
     
-    var isSwept = false
+    var isSwept = false {
+        
+        didSet {
+            
+            if self.mineCount > 0 {
+                
+                self.mineBackView.isHidden = !isSwept
+
+            }
+            else if self.mineCount == 0 {
+                
+                self.contentView.backgroundColor = .darkGray
+                
+                self.flagIcon.isHidden = true
+                
+            }
+            
+        }
+        
+    }
     
+    static func getBackColor(mineNumber: Int) -> UIColor {
+        
+        switch mineNumber {
+        case -1:
+            return .black
+        case 1:
+            return .green
+        case 2:
+            return .yellow
+        case 3:
+            return .orange
+        case 4:
+            return .red
+        case let n where n >= 5:
+            return .purple
+            
+        default:
+            return .white
+        }
+        
+    }
     
     
 }
--- Minesweeper/ViewController.swift
+++ Minesweeper/ViewController.swift
@@ -9,41 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let dimesional = 10
     
     var minesweeperArray: [[Int]]!
+    
+    var totlaMineNumber = 0
     
     @IBOutlet weak var minesColletcionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.createNewGame()
+        
+        self.addLongPressGesture()
+        
+    }
+    
+    private func createNewGame() {
+        
         self.createRandomMinesArray()
         
     }
-
+    
+    
     private func createRandomMinesArray() {
         
-        //採地雷
-        var bombCount = Int.random(in: 1 ... (dimesional * dimesional)/2 )
+        //random mineCount
+        var randomMineNumber = Int.random(in: (dimesional * dimesional)/10 ... (dimesional * dimesional)/4 )
+        
+        self.totlaMineNumber = randomMineNumber
         
         //1.產生內容全部為零的二維陣列
-       minesweeperArray  = createZeroArray(with: dimesional)
+        minesweeperArray  = createZeroArray(with: dimesional)
         
         //2.隨機挑選地雷座標，並將地雷數值填入
-        while bombCount > 0 {
+        while randomMineNumber > 0 {
             
-            var coordinate = getBombCoordinate()
+            var coordinate = randomMineCoordinate()
             
             while minesweeperArray[coordinate.0][coordinate.1] == -1 {
                 
-                coordinate = getBombCoordinate()
+                coordinate = randomMineCoordinate()
             }
             
             minesweeperArray[coordinate.0][coordinate.1] = -1
             
-            bombCount -= 1
+            randomMineNumber -= 1
             
         }
         
@@ -54,7 +67,7 @@ class ViewController: UIViewController {
                 
                 if minesweeperArray[rowIndex][columnIndex] == 0 {
                     
-                    let sum = countingBombSummary(row: rowIndex, column: columnIndex)
+                    let sum = summaryMine(row: rowIndex, column: columnIndex)
                     
                     minesweeperArray[rowIndex][columnIndex] = sum
                     
@@ -75,9 +88,15 @@ class ViewController: UIViewController {
             }
             print()
         }
-        
         print()
-
+    }
+    
+    private func addLongPressGesture() {
+        
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gesture:)))
+        
+        self.minesColletcionView.addGestureRecognizer(gesture)
+        
     }
     
     private func createZeroArray(with dimesional: Int) -> [[Int]] {
@@ -88,19 +107,19 @@ class ViewController: UIViewController {
         
     }
     
-    private func getBombCoordinate() -> (Int,Int) {
+    private func randomMineCoordinate() -> (Int,Int) {
         
-        let bombColumn = Int.random(in: 0 ..< dimesional)
+        let column = Int.random(in: 0 ..< dimesional)
         
-        let bombRow = Int.random(in: 0 ..< dimesional)
+        let row = Int.random(in: 0 ..< dimesional)
         
-        return (bombColumn, bombRow)
+        return (column, row)
         
     }
     
-    private func countingBombSummary(row: Int, column: Int ) -> Int {
+    private func summaryMine(row: Int, column: Int ) -> Int {
         
-        var bombCount = 0
+        var mineCount = 0
         
         let leftTop = (row - 1, column - 1)
         let top = (row - 1, column)
@@ -122,7 +141,7 @@ class ViewController: UIViewController {
                 
                 if minesweeperArray[row][column] == -1 {
                     
-                    bombCount += 1
+                    mineCount += 1
                     
                 }
                 
@@ -130,10 +149,10 @@ class ViewController: UIViewController {
             
         }
         
-        return bombCount
+        return mineCount
         
     }
-
+    
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -154,10 +173,17 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
             
             cell.mineCount = self.minesweeperArray[row][column]
             
+            // add a border
+            cell.layer.borderColor = UIColor.black.cgColor
+            
+            cell.layer.borderWidth = 1
+            
+            cell.backgroundColor = UIColor(red: 192.0/255.0, green: 192.0/255.0, blue: 192.0/255.0, alpha: 1)
+            
             return cell
             
         }
-    
+        
         return UICollectionViewCell()
         
     }
@@ -178,6 +204,224 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFl
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0.0
+    }
+    
+}
+
+
+extension ViewController: UICollectionViewDelegate {
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let tapCoordinate = Coordinate(indexPath: indexPath, globalDimesional: self.dimesional)
+        
+        let mineCount = self.minesweeperArray[tapCoordinate.row!][tapCoordinate.column!]
+        
+        if mineCount >= 0 {
+            
+            self.mineSweep(collectionView, coordinate: tapCoordinate)
+            
+            self.checkSweep()
+            
+            return
+            
+        }
+        
+        //點到炸彈
+        guard let cell = collectionView.cellForItem(at: indexPath) as? MineCell else { return }
+        
+        cell.mineIcon.isHidden = false
+        
+        self.presentAlert(titile: "點到炸彈", message: "再玩一次嗎?")
+        
+    }
+    
+    //handleLongPress
+    
+    @objc func handleLongPress(gesture : UILongPressGestureRecognizer!) {
+        
+        if gesture.state == .ended { return }
+        
+        if gesture.state == .cancelled { return }
+        
+        if gesture.state == .changed { return }
+        
+        let p = gesture.location(in: self.minesColletcionView)
+        
+        if let indexPath = self.minesColletcionView.indexPathForItem(at: p) {
+            
+            guard let cell = self.minesColletcionView.cellForItem(at: indexPath) as? MineCell else { return }
+            
+            if !cell.isSwept {
+                
+                cell.flagIcon.isHidden.toggle()
+                
+            }
+            
+        }
+        
+    }
+    
+    private func presentAlert(titile: String, message: String) {
+        
+        let alert = UIAlertController(title: titile, message: message, preferredStyle: .alert)
+        
+        let action_OK = UIAlertAction(title: "Yes", style: .default) { (action) in
+            
+            self.createNewGame()
+            
+            self.minesColletcionView.reloadData()
+            
+        }
+        
+        let action_cancel = UIAlertAction(title: "No", style: .cancel) { (action) in
+            
+            self.minesColletcionView.isUserInteractionEnabled = false
+            
+        }
+        
+        alert.addAction(action_OK)
+        
+        alert.addAction(action_cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+//MARK: Minesweep
+extension ViewController {
+    
+    private func mineSweep(_ collectionView: UICollectionView, coordinate: Coordinate, fromIndexPath: IndexPath? = nil) {
+        
+        guard let cell = collectionView.cellForItem(at: coordinate.indexPath!) as? MineCell else { return }
+        
+        guard cell.isSwept == false else { return }
+        
+        guard let row = coordinate.row,
+            0 ..< self.dimesional ~= row else { return }
+        
+        guard let column = coordinate.column,
+            0 ..< self.dimesional ~= column else { return }
+        
+        cell.isSwept.toggle()
+        
+        let myMineCount = self.minesweeperArray[row][column]
+        
+        if myMineCount > 0 { return }
+        
+        if myMineCount == 0 {
+            
+            cell.layer.borderWidth = 0
+            
+            cell.mineBackView.backgroundColor = .darkGray
+            
+        }
+        
+        let leftTop = Coordinate(row: row - 1, column: column - 1, globalDimesional: self.dimesional)
+        
+        let top = Coordinate(row: row - 1, column: column, globalDimesional: self.dimesional)
+        
+        let rightTop = Coordinate(row: row - 1, column: column + 1, globalDimesional: self.dimesional)
+        
+        let left = Coordinate(row: row, column: column - 1, globalDimesional: self.dimesional)
+        
+        let right = Coordinate(row: row, column: column + 1, globalDimesional: self.dimesional)
+        
+        let leftBottom = Coordinate(row: row + 1, column: column - 1, globalDimesional: self.dimesional)
+        
+        let bottom = Coordinate(row: row + 1, column: column, globalDimesional: self.dimesional)
+        
+        let rightBottom = Coordinate(row: row + 1, column: column + 1, globalDimesional: self.dimesional)
+        
+        let surroundCoordinates = [leftTop, top, rightTop, left, right, leftBottom, bottom, rightBottom]
+        
+        if let fromIndexPath = fromIndexPath {
+            
+            let fromCoordinate = Coordinate(indexPath: fromIndexPath, globalDimesional: self.dimesional)
+            
+            for surroundCoordinate in surroundCoordinates {
+                
+                if surroundCoordinate == fromCoordinate {
+                    
+                    continue
+                    
+                }
+                
+                self.mineSweep(collectionView, coordinate: surroundCoordinate, fromIndexPath: coordinate.indexPath!)
+                
+            }
+            
+        }
+        else {
+            
+            for surroundCoordinate in surroundCoordinates {
+                
+                self.mineSweep(collectionView, coordinate: surroundCoordinate, fromIndexPath: coordinate.indexPath!)
+                
+            }
+            
+        }
+        
+    }
+    
+    func checkSweep() {
+        
+        var sweptCount = 0
+        
+        let indexPaths = self.minesColletcionView.indexPathsForVisibleItems
+        
+        for indexPath in indexPaths {
+            
+            guard let cell = self.minesColletcionView.cellForItem(at: indexPath) as? MineCell else { return }
+            
+            if cell.isSwept {
+                
+                sweptCount += 1
+                
+            }
+            
+        }
+        
+        let isOverCount = (dimesional * dimesional) - self.totlaMineNumber
+        
+        if sweptCount == isOverCount {
+            
+            self.presentAlert(titile: "恭喜過關!!", message: "再玩一次嗎？")
+            
+        }
+        
+        
+    }
+    
+    struct Coordinate: Equatable {
+        
+        var row: Int?
+        
+        var column: Int?
+        
+        var indexPath: IndexPath?
+        
+        init(indexPath: IndexPath, globalDimesional: Int) {
+            
+            self.row = indexPath.row / globalDimesional
+            
+            self.column = indexPath.row - (globalDimesional * self.row!)
+            
+            self.indexPath = indexPath
+            
+        }
+        
+        init(row: Int, column: Int, globalDimesional: Int) {
+            
+            self.row = row
+            
+            self.column = column
+            
+            self.indexPath = IndexPath(row: row * globalDimesional + column, section: 0)
+            
+        }
+        
     }
     
 }
